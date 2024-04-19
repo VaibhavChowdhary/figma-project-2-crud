d6ef3d3753b378019a5c282a88d8cab72cdb40de
Vaibhav Chowdhary <86661832+VaibhavGC2k@users.noreply.github.com>
2024-04-19 10:41:58 +0000

    hello

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -5,22 +5,23 @@ import { SomeContext } from '../context/context'
 import { Route, Routes } from 'react-router-dom';
 import { Box } from '@mui/material';
 import Home from '../pages/Home';
+import Payment from '../pages/Payment';
 
 export default function Dashboard() {
     const [openSidebar, setOpenSidebar] = useState(false);
     return (
         <>
-            <Box sx={{ display: 'flex', justifyContent: "start" }}>
+            <Box sx={{ display: 'flex', justifyContent: "start", backgroundColor: "#F8F8F8",height:"100vh" }}>
                 <SomeContext.Provider value={{ openSidebar, setOpenSidebar }}>
                     <Sidebar />
                     <Header />
                 </SomeContext.Provider>
-                <Box sx={{ height: "30vh", width: openSidebar ? "100vw" : "80vw", display: "flex", justifyContent: "center", alignItems: "center" }}>
+                <Box sx={{ height: "50vh", width: openSidebar ? "100vw" : "80vw", display: "flex", justifyContent: "center", alignItems: "center" }}>
                     <Routes>
                         <Route path='/home' element={<Home />} />
                         <Route path='/course' element={<h1>Course</h1>} />
                         <Route path='/students' element={<h1>Students</h1>} />
-                        <Route path='/payments' element={<h1>Payments</h1>} />
+                        <Route path='/payments' element={<Payment />} />
                         <Route path='/report' element={<h1>Report</h1>} />
                         <Route path='/settings' element={<h1>Settings</h1>} />
                     </Routes >
---
+++ src/pages/Payment.jsx
@@ -0,0 +1,108 @@
+import { Box, Divider } from '@mui/material'
+import React from 'react'
+import CustomTypo from '../CustomComponents/CustomTypo'
+import filter from "../assets/filter.png"
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import eye from "../assets/eye.png"
+export default function Payment() {
+    const rows = [
+        { name: "vaibhav", paymentSchedule: "first", billNumber: "00012223", amountPaid: "INR 35,000", balanceAmount: "INR 55,000", date: "08 Dec, 2021" },
+        { name: "vaibhav", paymentSchedule: "first", billNumber: "00012223", amountPaid: "INR 35,000", balanceAmount: "INR 55,000", date: "08 Dec, 2021" },
+        { name: "vaibhav", paymentSchedule: "first", billNumber: "00012223", amountPaid: "INR 35,000", balanceAmount: "INR 55,000", date: "08 Dec, 2021" },
+    ]
+    return (
+        <>
+            <Box sx={{ width: "100%", padding: "30px" }}>
+                <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+                    <CustomTypo fontWeight="bold" fontSize="22px" color="#000000">
+                        Payment Details
+                    </CustomTypo>
+                    <img src={filter} alt='filter' width="14px" height="19.25px" />
+                </Box>
+                <Divider sx={{ color: "#E5E5E5", marginTop: "20px", marginBottom: "18px" }} />
+                <Table sx={{ minWidth: "1300px" }} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell sx={{ border: "none" }}>
+                                <CustomTypo color="#ACACAC" fontSize="12px" >
+                                    Name
+                                </CustomTypo>
+                            </TableCell>
+                            <TableCell sx={{ border: "none" }}>
+                                <CustomTypo color="#ACACAC" fontSize="12px" >
+                                    Payment Schedule
+                                </CustomTypo>
+                            </TableCell>
+                            <TableCell sx={{ border: "none" }} >
+                                <CustomTypo color="#ACACAC" fontSize="12px" >
+                                    Bill Number
+                                </CustomTypo>
+                            </TableCell>
+                            <TableCell sx={{ border: "none" }} >
+                                <CustomTypo color="#ACACAC" fontSize="12px" >
+                                    Amount Paid
+                                </CustomTypo>
+                            </TableCell>
+                            <TableCell sx={{ border: "none" }}>
+                                <CustomTypo color="#ACACAC" fontSize="12px" >
+                                    Balance amount
+                                </CustomTypo>
+                            </TableCell>
+                            <TableCell sx={{ border: "none" }} >
+                                <CustomTypo color="#ACACAC" fontSize="12px" >
+                                    Date
+                                </CustomTypo>
+                            </TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows.map((row, index) => (
+                            <TableRow
+                                key={index}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 }, backgroundColor: index % 2 === 0 ? "#FFFFFF" : "#F8F8F8" }}
+                            >
+                                <TableCell sx={{ border: "none" }}>
+                                    <CustomTypo color="#000000" fontSize="14px" fontWeight="500">
+                                        {row.name}
+                                    </CustomTypo>
+                                </TableCell>
+                                <TableCell align="left" sx={{ border: "none" }}>
+                                    <CustomTypo color="#000000" fontSize="14px" fontWeight="500">
+                                        {row.paymentSchedule}
+                                    </CustomTypo>
+                                </TableCell>
+                                <TableCell align="left" sx={{ border: "none" }}>
+                                    <CustomTypo color="#000000" fontSize="14px" fontWeight="500" >
+                                        {row.billNumber}
+                                    </CustomTypo>
+                                </TableCell>
+                                <TableCell align="left" sx={{ border: "none" }}>
+                                    <CustomTypo color="#000000" fontSize="14px" fontWeight="500" >
+                                        {row.amountPaid}
+                                    </CustomTypo>
+                                </TableCell>
+                                <TableCell align="left" sx={{ border: "none" }}>
+                                    <CustomTypo color="#000000" fontSize="14px" fontWeight="500">
+                                        {row.balanceAmount}
+                                    </CustomTypo>
+                                </TableCell>
+                                <TableCell align="left" sx={{ border: "none" }}>
+                                    <CustomTypo color="#000000" fontSize="14px" fontWeight="500">
+                                        {row.date}
+                                    </CustomTypo>
+                                </TableCell>
+                                <TableCell align="right" sx={{ border: "none" }}>
+                                    <img src={eye} alt='view' />
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </Box>
+        </>
+    )
+}
